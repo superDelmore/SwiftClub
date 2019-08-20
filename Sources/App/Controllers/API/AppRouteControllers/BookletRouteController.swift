60fdf8006e4802e7f6f11219d1adc4df6dab68b9
jihua <1164258202@qq.com>
2019-08-20 09:05:40 +0800

    Feat: 添加目录接口声明

--- Sources/App/Controllers/API/AppRouteControllers/BookletRouteController.swift
+++ Sources/App/Controllers/API/AppRouteControllers/BookletRouteController.swift
@@ -8,24 +8,55 @@
 import Vapor
 
 final class BookletRouteController: RouteCollection {
+    private let menuService = MenuService()
+
     func boot(router: Router) throws {
         let group = router.grouped("booklet")
 
         let guardAuthMiddleware = User.guardAuthMiddleware()
         let tokenAuthMiddleware = User.tokenAuthMiddleware()
         let tokenAuthGroup = group.grouped([tokenAuthMiddleware, guardAuthMiddleware])
+
         // 小册列表
         group.get("list", use: bookletList)
-        // 添加
-        tokenAuthGroup.post(BookletReqContainer.self, at:"add", use: bookletAdd)
 
-        /// 目录
+        // 小册目录树
         group.get("catalog", use: catalogDetail)
+
+        // 小册添加
+        tokenAuthGroup.post(BookletReqContainer.self, at: "add",  use: bookletAdd)
+
+        // 小册更新
+        tokenAuthGroup.post(BookletReqContainer.self, at: "update", use: bookletUpdate)
+
+        // 小册删除
+        tokenAuthGroup.post("delete", use: bookletDelete)
+
+        // 目录更新
+        tokenAuthGroup.post(CatalogReqContainer.self, at:"catalog", "update", use: catalogUpdate)
+
+        // 目录添加
+        tokenAuthGroup.post(CatalogReqContainer.self, at: "catalog", "add", use: catalogAdd)
+
+        // 目录删除
+        tokenAuthGroup.post("catalog", "delete", use: catalogDelete)
 
     }
 }
 
 extension BookletRouteController {
+
+    func catalogAdd(request: Request, container: CatalogReqContainer) throws -> Future<Response> {
+        return try request.makeJson()
+    }
+
+    func catalogDelete(request: Request) throws -> Future<Response> {
+        return try request.makeJson()
+    }
+
+    func catalogUpdate(request: Request, container: CatalogReqContainer) throws -> Future<Response> {
+        return try request.makeJson()
+    }
 
     func catalogDetail(request: Request) throws -> Future<Response> {
         return try request.makeJson()
@@ -51,6 +82,14 @@ extension BookletRouteController {
                                   userId: container.userId)
             return try booklet.create(on:request).makeJson(on: request)
         }
+    }
+
+    func bookletUpdate(request: Request, container: BookletReqContainer) throws -> Future<Response> {
+         return try request.makeJson()
+    }
+
+    func bookletDelete(request: Request) throws -> Future<Response> {
+        return try request.makeJson()
     }
 
 
--- Sources/App/Models/Requests/BookletReqContainers.swift
+++ Sources/App/Models/Requests/BookletReqContainers.swift
@@ -12,3 +12,13 @@ struct BookletReqContainer: Content {
     var desc: String?
     var userId: User.ID
 }
+
+struct CatalogReqContainer: Content {
+    var id: Int?
+    var pid: Catalog.ID  // 一级菜单，默认为0；如果是二级菜单，则为一级菜单id
+    var title: String
+    var path: String? // 表示父级id列表
+    var level: Int // 层级, 辅助字段，记录该菜单是第几级
+    var order: Int // 权重，用于同一等级的菜单之间的排序, 数字越大，越靠前
+    var topicId: Topic.ID? // 文章 id
+}
